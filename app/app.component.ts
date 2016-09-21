ee4affa7f4937ef50bf27a8f1d2204dfa0c8a533
javalv <java.com.lv@163.com>
2016-09-21 19:02:02 +0800

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+// import '../../public/css/styles.css';
+@Component({
+    selector: 'my-app',
+    template: `
+        <main>
+        <h1>Hello from Angular 2 App with Webpack</h1>
+        <nav>
+            <a routerLink="/home" >home</a>
+            <a routerLink="/page1" >page12</a>
+        </nav>
+        <router-outlet></router-outlet>
+        </main>`,
+
+})
+export class AppComponent {
+}
